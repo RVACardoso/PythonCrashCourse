ecef6eca46e584d18285096d08c075bf5113124b
RVACardoso <valentim.ruben@gmail.com>
2019-11-16 19:52:16 +0000

    Update 4-interpol.py

--- 4-interpol.py
+++ 4-interpol.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 from scipy import interpolate
 import numpy as np
-# %matplotlib auto
-# %matplotlib inline
 
     # 1D Interpolation
 
@@ -16,12 +14,11 @@ plt.show()
 # string: ‘linear’, ‘nearest’, ‘zero’, ‘slinear’, ‘quadratic’, ‘cubic’
 # where ‘zero’, ‘slinear’, ‘quadratic’ and ‘cubic’ refer to a spline 
 # interpolation of zeroth, first, second or third order
-# or integer specifying the order of the spline interpolator to use
 f = interpolate.interp1d(x, y, kind='quadratic')
 xnew = np.arange(0, 9, 0.01)    
 ynew = f(xnew)
 plt.scatter(x,y, color='blue')
-plt.plot(xnew,ynew, color='green')
+plt.plot(xnew,ynew, color='green', lw=2)
 plt.show()
 
 
@@ -31,18 +28,22 @@ print("\n 2D Interpolation")
 def func(x, y):
     return (x+y)*np.exp(-2.0*(x**2 + y**2))
 
-plt.figure()
 x, y = np.mgrid[-1:1:25j, -1:1:25j] # create 25 points including stop
 fval = func(x, y)
-plt.pcolormesh(x, y, fval, cmap='rainbow')
 
 from mpl_toolkits.mplot3d import Axes3D
 
+# 3D plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d') # nrows, ncols, and index
 sf = ax.plot_surface(x, y, fval, cmap='jet')
-fig.colorbar(sf, shrink=0.5)
+fig.colorbar(sf)
 
+# level plot
+plt.figure()
+plt.pcolormesh(x, y, fval, cmap='rainbow')
+
+# interpolation
 plt.figure()
 f = interpolate.interp2d(x, y, fval, kind='cubic')
 xn = np.arange(-1, 1, 0.01)
@@ -52,10 +53,11 @@ plt.pcolormesh(xn, yn, f(xn, yn), cmap='rainbow')
 
     # Data smoothing
 
+#plt.rc("lines", lw=3, ls="-")
 plt.figure()
 x = np.arange(-5, 5, 0.1)
 y = np.sin(x) + 0.25*np.random.rand(len(x))
-plt.plot(x,y)
+plt.plot(x,y, lw=3, ls="-")
 
 # Savitzky-Golay: https://en.wikipedia.org/wiki/Savitzky%E2%80%93Golay_filter#/media/File:Lissage_sg3_anim.gif
 
@@ -63,27 +65,24 @@ from scipy.signal import savgol_filter
 
 ysg = savgol_filter(y, window_length=11, polyorder=2)
 plt.figure()
-plt.plot(x, y, label='Original')
-plt.plot(x, ysg, label='Savitzky-Golay')
+plt.plot(x, y, label='Original', ls=":")
+plt.plot(x, ysg, label='Savitzky-Golay', ls="-")
 plt.legend()
 
 
-    #   Cubic spline
+    #   1D interpolation: Cubic spline
     
 plt.figure()
 x = np.arange(10)
 y = np.sin(x)
-plt.scatter(x,y, color='blue')
-plt.plot()
+plt.scatter(x,y, color="red")
 
 cs = interpolate.CubicSpline(x, y)
 xs = np.arange(-0.5, 9.6, 0.1)
  
-plt.plot(x, y, 'o', label='data')
-plt.plot(xs, np.sin(xs), label='true')
-plt.plot(xs, cs(xs), label="S")
-plt.xlim(-0.5, 9.5)
-plt.legend(loc='lower left', ncol=2)
+plt.plot(xs, np.sin(xs), label='true', lw=5, ls="--")
+plt.plot(xs, cs(xs), label="cubic spline", lw=2, ls="-")
+plt.legend(loc='lower left')
 
 
 
