acb7ba224c547d3ca9f5f918513d763a2249de66
RVACardoso <valentim.ruben@gmail.com>
2019-11-16 19:53:40 +0000

    Update 7-spectra.py

--- 7-spectra.py
+++ 7-spectra.py
@@ -4,14 +4,15 @@ from scipy import signal
 
 
 t = np.linspace(0.0, 10, 5001)
-fs = 5001.0/10.0
-sig = signal.chirp(t, f0=10, f1=20, t1=10, method='linear')
+sig = signal.chirp(t, f0=1, t1=10, f1=30, method='linear')
 plt.figure()
 plt.plot(t, sig)
 plt.title("Signal in time domain")
 
 plt.figure()
+fs = 5001.0/10.0
 f, t, Sxx = signal.spectrogram(sig, fs, nfft=2048, nperseg=128)
+
 plt.pcolormesh(t, f, Sxx)
 plt.ylabel('Frequency')
 plt.xlabel('Time')
