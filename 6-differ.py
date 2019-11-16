c39f3e5027d5329c33d9a85b5060069115782be8
RVACardoso <valentim.ruben@gmail.com>
2019-11-16 19:53:16 +0000

    Update 6-differ.py

--- 6-differ.py
+++ 6-differ.py
@@ -8,14 +8,15 @@ def f(x, a, b, c, d):
     return a * np.sin(b*x + c) + d
 
 a, b, c, d = 1.0, 4.0, 3.0, 1.0
-arr = (a, b, c, d)
 dstep = 0.01
 x = np.arange(-5, 5, dstep)
-y = f(x, *arr)
+y = f(x, a, b, c, d)
 plt.figure()
 plt.plot(x, y, color='b')
+
 plt.plot(x, np.gradient(y, dstep), color='r') #works for n-D functions, returns matrix
 plt.title("Red is derivative of blue")
+plt.show()
 
     # Numeric Integration 
 
@@ -24,10 +25,10 @@ y = x
 yn = x + 0.1*np.random.randn(len(x))
 plt.figure()
 plt.plot(x ,yn)
+plt.scatter(x ,yn)
 plt.title("Integrand function")
+plt.show()
 
 print("\nNumpy trapezoidal rule: {}".format(np.trapz(yn, x=x)))
 print("SciPy trapezoidal rule: {}".format(integrate.trapz(yn, x=x)))
 print("SciPy Simpson rule: {}".format(integrate.simps(yn, x=x)))
-
-
