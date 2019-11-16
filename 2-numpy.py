67bc811c2bfca85c3c16076e29a39d4534819f37
RVACardoso <valentim.ruben@gmail.com>
2019-11-16 19:50:48 +0000

    Update 2-numpy.py

--- 2-numpy.py
+++ 2-numpy.py
@@ -7,11 +7,15 @@ print("\n Array A:\n {}".format(A))
 
 line1 = [1,2,3]
 line2 = [4,5,6]
-B = np.array([line1, line2])
+mat_list = [line1, line2]
+B = np.array(mat_list)
 print("\n Array B:\n {}".format(B))
 
-spaced = np.arange(2,29,3) #includes first, not last
-print("\n Equally spaced:\n {}".format(spaced))
+spaced1 = np.arange(2,29,3) #includes first, not last
+print("\n Equally spaced given spacing:\n {}".format(spaced1))
+
+spaced2 = np.linspace(2,29,13) #includes first and last
+print("\n Equally spaced given spacing:\n {}".format(spaced2))
 
 print("\n 0 filled matrices: ")
 zero = np.zeros((2,3))
@@ -32,7 +36,7 @@ print("\n Get shape")
 print(M.shape)
 
 print("\n Reshape Array: ")
-Res = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9] )
+Res = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 print(Res)
 Res = Res.reshape(3,3)
 print(Res)
